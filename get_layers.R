9550438269a4f0ef6c32d99a0e8e50f8ae00b23f
Viliam Simko <viliam.simko@gmail.com>
2016-06-24 18:31:41 +0200

    added R script with argparser support

---
+++ get_layers.R
@@ -0,0 +1,22 @@
+#!/usr/bin/Rscript
+
+library(methods)
+library(utils)
+library(argparser)
+
+p <- arg_parser("This script downloads a list of available layers from a geoserver")
+
+p <- add_argument(p, "URL",  help = "
+                  Base URL of the Geoserver.")
+
+p <- add_argument(p, "--keep-xml", help = "
+                  If specified, the orignal XML from Geoserver will also be
+                  stored.")
+
+p <- add_argument(p, "--output", help = "
+                  By default, the output will be printed to standard output.
+                  This parameter specifies an alternative location, where the
+                  results should be stored.")
+
+argv <- parse_args(p)
+print(argv)
