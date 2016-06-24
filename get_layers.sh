f48a5b20f746118fea03c733d08b016dc1d046d7
kelaub <info@kevin-laubis.de>
2016-06-24 15:33:38 +0200

    download raw xml capabilities of geo server

--- get_layers.sh
+++ get_layers.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# $1 url for getting capabilities of geoserver (e.g. http://mygeoserver.com/geoserver/wms?request=GetCapabilities&service=WMS)
 
-# download list of layers
+# download xml list of layers
+wget $1 --output-document='layers.xml'
+
+# select layer names
+xmllint --xpath '//Layer/Name' layers.xml
+
 # update list of layers
 # print report
