7140227fe767564dd938c466ab3f9a97d881e941
kelaub <info@kevin-laubis.de>
2016-06-29 17:59:18 +0200

    surpressed some print commands

--- download_gml.R
+++ download_gml.R
@@ -8,26 +8,27 @@
 
 library(methods)
 library(argparser)
+library(httr)
 
 p <- arg_parser("This script downloads a random layer from the list")
 
 p <- add_argument(p, "URL",  help = "
-                  Base URL of the Geoserver.")
+                  Base URL of the Geoserver (e.g. 'http://map.mygeoserver.com/geoserver').")
 
-p <- add_argument(p, "DIR",  help = "
+p <- add_argument(p, "DIR", help = "
                   Output directory, where the downloaded layers will be stored")
 
-p <- add_argument(p, "--layers", type = , help = "
+p <- add_argument(p, "--layers", type = , default = "layers.log", help = "
                   File containing the names of layers.
                   Single layer name per line.")
 
-#argv <- parse_args(p)
+argv <- parse_args(p)
 #print(argv)
-argv <- list(
-  URL = "http://map.bumprecorder.com/geoserver",
-  DIR = "testdir",
-  layers = "dummy-layers.txt"
-)
+# argv <- list(
+#   # URL = "http://map.mygeoserver.com/geoserver",
+#   DIR = "testdir",
+#   layers = "layers.log"
+# )
 
 if ( is.na(argv$layers) ) {
   argv$layers <- "/dev/stdin"
@@ -40,11 +41,13 @@ cat(sprintf("Layer names:   %s\n", argv$layers))
 # list required layers
 alllayers <- unlist(read.table(argv$layers, stringsAsFactors = FALSE))
 print(alllayers)
+# print(alllayers)
 
 # list already downloaded layers
 donelayers <- list.files(argv$DIR)
 donelayers <- sub("\\.xml$", "", donelayers)
 print(donelayers)
+# print(donelayers)
 
 # list layers remaining
 setdiff(alllayers, donelayers)
