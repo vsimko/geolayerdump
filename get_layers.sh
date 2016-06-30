df4cf493ec930699d6ec2f1ee963a709d073d1e0
Viliam Simko <viliam.simko@gmail.com>
2016-06-30 15:46:50 +0200

    get_layers implemented quickly in shell

--- get_layers.sh
+++ get_layers.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
-# $1 url for getting capabilities of geoserver (e.g. http://mygeoserver.com/geoserver/wms?request=GetCapabilities&service=WMS)
 
-# download xml list of layers
-wget $1 --output-document='layers.xml'
+function usage() {
+  echo USAGE: $(basename "$0") URL DIR
+  exit 1
+}
 
-# select layer names
-xmllint --xpath '//Layer/Name' layers.xml
+URL="$1" # e.g. http://mygeoserver.com/geoserver/wms?request=GetCapabilities&service=WMS
+DIR="$2" # e.g. /path/to/my/mydir
 
-# update list of layers
-# print report
+[[ -z "$1" ]] && usage
+[[ -z "$2" ]] && usage
+
+mkdir --parents "$DIR"
+LAYERS=`mktemp list_of_layers.XXXX`
+
+# Quick and dirty XML parsing.
+# we should later use e.g. xmllint --xpath '//Layer/Name' layers.xml
+wget -O /dev/stdout "$URL/wms?request=GetCapabilities&service=WMS" |
+  grep -A 1 '<Layer' |
+  grep Name |
+  sed -e 's/.*<Name>//g' -e 's/<\/Name>//g' |
+  sort -u | awk '{print $1".kml"; print $1".gml"}' > "$LAYERS"
+
+# Creating new layers
+while read FNAME; do
+  touch "$DIR/$FNAME"
+done < "$LAYERS"
+
+# Removing old layers
+# Note: this is how set complement is implemented in shell
+comm -23 <(ls -1 "$DIR" | sort) <(sort "$LAYERS") |
+  while read FILE; do
+    rm "$DIR/$FILE"
+  done
+
+# cleanup temp files
+rm "$LAYERS"
+
+echo done.
