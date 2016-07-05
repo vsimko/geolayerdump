e0bc70aa2a3f156e0c1d39bde4b17d9ae0ffb390
Viliam Simko <viliam.simko@gmail.com>
2016-07-05 15:08:46 +0200

    implemented the shell script for downloading a single layer from geoserver

--- download_layer.sh
+++ download_layer.sh
@@ -5,13 +5,77 @@ function usage() {
   exit 1
 }
 
-URL="$1" # e.g. http://mygeoserver.com/geoserver/wms?request=GetCapabilities&service=WMS
+RETRY_LIMIT=3
+DOWNLOAD_BEGIN="NEVER"
+DOWNLOAD_END="NEVER"
+
+URL="$1" # e.g. http://mygeoserver.com/geoserver/
 DIR="$2" # e.g. /path/to/my/mydir
 
+# check parameters and show usage
 [[ -z "$1" ]] && usage
 [[ -z "$2" ]] && usage
 
-find "$DIR" -daystart -mtime 0 -printf "%T@ %f\n" |
-  sort -r | head -1 | while read OLDEST; do
+TMP=`mktemp geo-download.XXXXX`
 
-  done
+# list all files, but just GML files
+# filter: we need files older than 1 day (-daystart \! -mtime 0)
+# order files by mtime (oldest on top)
+# if there is no oldest file, just finish
+find "$DIR" -name '*.gml' -daystart \! -mtime 0 -printf "%T@ %f\n" |
+sort | head -1 | while read OLDEST; do
+  FILENAME="${OLDEST/#*.* /}"
+  TYPENAME="${FILENAME%.gml}"
+  SAVETOFILE="$DIR/$FILENAME"
+  METANAME="$DIR/$TYPENAME.meta"
+  echo "Oldest file: $OLDEST"
+
+  # try to download the oldest file
+  # if failed to download, try it N times (keep the counter in a separate file)
+  DOWNLOAD_URL="${URL%/}/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=$TYPENAME"
+  echo "DOWNLOAD: $DOWNLOAD_URL to $SAVETOFILE"
+
+  # loading metadata sidecar file
+  if [ -f "$METANAME" ]; then
+    source "$METANAME"
+  fi
+
+  # count the number of download retries
+  DOWNLOAD_COUNT=$((DOWNLOAD_COUNT + 1))
+
+  # timestamp of the beginning of download
+  TMPBEGIN=`date '+%Y-%m-%d %H:%M:%S'`
+
+  if wget --output-document="$TMP" "$DOWNLOAD_URL" --limit-rate=1m
+  then
+    echo -n "Download successful, now formatting XML ... "
+    xmllint --format "$TMP" | sed 's/\s*fid="[^"]*"//' > "$SAVETOFILE"
+    echo "ok"
+
+    # preparing timestamps to be stored as metadata
+    DOWNLOAD_BEGIN="$TMPBEGIN"
+    DOWNLOAD_END=`date '+%Y-%m-%d %H:%M:%S'`
+
+  else
+
+    echo "Error downloading the layer (counter=$DOWNLOAD_COUNT)"
+
+    # If an upper limit of download retries is reached,
+    # we skip the file and try to download it the next day.
+    if [ "$DOWNLOAD_COUNT" -gt "$RETRY_LIMIT" ]; then
+
+      # We keep the content from the last successful download
+      touch "$SAVETOFILE"
+
+      # reseting the counter
+      DOWNLOAD_COUNT=0
+      echo "Retry limit reached. We'll try to download this layer tomorrow."
+    fi
+  fi
+
+  # store updated metadata in a sidecar file
+  typeset -p DOWNLOAD_COUNT DOWNLOAD_BEGIN DOWNLOAD_END > "$METANAME"
+done
+
+rm "$TMP"
+echo "done"
